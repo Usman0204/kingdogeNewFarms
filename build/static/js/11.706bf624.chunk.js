c3fc24034ff17417347e77eaa53ec4e9a23e2168
waleed-shafiq <waleed.shafiq96@gmail.com>
2021-08-10 21:33:43 +0500

    deploy

---
+++ build/static/js/11.706bf624.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[11],{1392:function(n,p){}}]);
+//# sourceMappingURL=11.706bf624.chunk.js.map
--- build/static/js/7.58550501.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppancake-frontend"]=this["webpackJsonppancake-frontend"]||[]).push([[7],{1291:function(n,p){}}]);
-//# sourceMappingURL=7.58550501.chunk.js.map
